fceb969bc495e205f553e0cce61d47714aca2f12
crstofrd <christogumerung@gmail.com>
2025-11-25 11:22:26 +0800

    Implement main function for simple calculator
    Add main function for a simple calculator with options.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#Nantinya akan import fungsi operations dan input_handler
 
+def main():
+  while True:
+    print("\n=== Kalkulator Sederhana ===")
+    print("1. Tambah")
+    print("2. Kurang")
+    print("3. Kali")
+    print("4. Bagi")
+    print("5. Keluar")
+    
+    pilihan = input("Pilih operasi (1-5): ")
+    
+    if pilihan == '5':
+      print("Terima kasih telah menggunakan kalkulatorr kami!")
+      break
