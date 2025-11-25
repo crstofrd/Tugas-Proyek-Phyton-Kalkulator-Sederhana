c77b55623fdd67c3db34b84d7aec6ae8dd6a85fb
Hera28 <116450997+Hera28@users.noreply.github.com>
2025-11-25 13:14:42 +0800

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,4 +1,5 @@
 #operation.py
+#catatan tambahan: kode dalam file ini kami #copy paste dari repository yang ada sebelum #nya ini yang dimana repositort tersebut kami #telah hapus 
 
 def tambah(a, b): return a + b
 def kurang(a, b): return a - b
@@ -6,3 +7,4 @@ def kali(a, b): return a * b
 def bagi(a, b): if b == 0:
 return "Error: Tidak bisa membagi dengan nol!"
 return a / b
+
